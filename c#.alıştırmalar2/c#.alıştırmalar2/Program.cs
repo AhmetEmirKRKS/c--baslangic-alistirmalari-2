6d5906ca61c464823cd255ff1116683abfceabbd
AhmetEmirKRKS <ahmetemirkrks001@gmail.com>
2023-09-12 16:42:43 +0300

    c# baslangic alistirmalari
    c# temel seviye konsol örnekleri

---
+++ c#.alıştırmalar2/c#.alıştırmalar2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_.alıştırmalar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin1 = "True";
+            bool sb1=bool.Parse(metin1); 
+            bool sb2=Convert.ToBoolean(metin1);
+            Console.WriteLine(metin1);
+            int s1 = 100;
+         byte byte1=(byte)s1;
+            float float1 = s1;
+            Console.WriteLine(s1);
+            byte b1 = 10;
+            decimal d1 = b1;
+            Console.WriteLine(d1);
+            Console.ReadLine();
+        }
+    }
+}
